23e0f9c76117bdaad4935b1744cf4483efc4d839
Vlad Marian Cimpeanu <62434812+VladMarianCimpeanu@users.noreply.github.com>
2022-11-14 21:28:50 +0100

    added function to create oversampling training set

--- multiclassification_task/utils/utils.py
+++ multiclassification_task/utils/utils.py
@@ -1,8 +1,12 @@
 import splitfolders
 import os
-import tensorflow as tf
+#import tensorflow as tf
 import pandas as pd
 import numpy as np
+import shutil
+import random
+import time
+
 
 METADATA = os.path.dirname(os.path.abspath(__file__)) + "/../metadata_validation.txt"
 
@@ -62,5 +66,36 @@ def build_heatmap(model, validation_generator):
     df_heatmap = pd.DataFrame(c)
     return df_heatmap
 
+
+def over_sampling():
+    # pointing to the right folder
+    relative_path = os.path.dirname(os.path.abspath(__file__))
+    train_path = f"{relative_path}\\..\\train"
+    labels = os.listdir(train_path)
+
+    counts = {}
+    # computing frequency for each class
+    for label in labels:
+        counts[label] = len(os.listdir(f"{train_path}/{label}"))
+        print(f"visited {label}")
+    highest_frequency = max(counts.values())
+
+    # creating new training folder with oversampling
+    os.mkdir(f"{relative_path}\\..\\train_oversampled")
+    for label in labels:
+        os.mkdir(f"{relative_path}\\..\\train_oversampled\\{label}")
+        # copy subdirectory 
+        from_directory = f"{relative_path}\\..\\train\\{label}"
+        to_directory = f"{relative_path}\\..\\train_oversampled\\{label}"
+        files = os.listdir(from_directory)
+        for img in files:
+            shutil.copy(f"{from_directory}\\{img}", to_directory)
+        if counts[label] < 0.8 * highest_frequency: # acceptable threashold
+            for _ in range(highest_frequency - counts[label]):
+                sample = random.choice(files)
+                shutil.copyfile(f"{to_directory}\\{sample}", f"{to_directory}\\{random.randint(0, 10000)}{sample}")
+            print(label, len(os.listdir(to_directory)), counts[label], highest_frequency)
+            
+
 if __name__ == "__main__":
-    split_folders(0.8)
+    over_sampling()
